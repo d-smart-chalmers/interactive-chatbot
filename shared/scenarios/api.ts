d02a634a95f03006d24881cafd28fd450c4c0b18
Jesper Nyberg <jesperny@chalmers.se>
2026-03-19 12:25:36 +0100

    feat: shared types and interfaces

--- shared/scenarios/api.ts
+++ shared/scenarios/api.ts
@@ -1,5 +1,14 @@
-import type { ScenarioDescription } from "./model";
+import type { ScenarioChatHistory, ScenarioDescription, UserRole } from "./model";
 
 export interface DescriptionsResponse {
   descriptions: ScenarioDescription[][];
+}
+
+export interface StartScenarioRequest {
+  userRole: UserRole;
+}
+
+export interface StartScenarioResponse {
+  description: string;
+  history: ScenarioChatHistory;
 }
--- shared/scenarios/model.ts
+++ shared/scenarios/model.ts
@@ -2,3 +2,32 @@ export interface ScenarioDescription {
     id: string,
     description: string,
 }
+
+export enum UserRole {
+    Vessel = "vessel",
+    VTS = "vts",
+}
+export enum TurnType {
+    User = "user",
+    Chatbot = "chatbot",
+}
+
+export interface ScenarioChatHistory{
+    turns: TurnHistory[],
+    intruction: string,
+}
+export interface BaseTurn{
+    message: string,
+    timestamp: Date,
+    type: TurnType,
+
+}
+export interface UserTurn extends BaseTurn{
+    feedback: string,
+    correct: boolean,
+}
+export interface ChatbotTurn extends BaseTurn{
+
+}
+
+export type TurnHistory = UserTurn | ChatbotTurn
