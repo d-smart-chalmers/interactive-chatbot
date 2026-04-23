25097e17dbc8cd7f17171a70889d23b6f4d16f5d
Jesper Nyberg <jesperny@chalmers.se>
2026-04-23 12:18:35 +0200

    chore: clean up of console.logs

--- client/app/dashboard/Dashboard.tsx
+++ client/app/dashboard/Dashboard.tsx
@@ -34,7 +34,6 @@ export default function Dashboard() {
     }
     hasFetched.current = true;
     fetchData();
-    console.log('fetching data');
   }, []);
 
   const onClickScenario = (id: string) => {
